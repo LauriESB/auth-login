c35e0c0337cea564d27b7dc1915ae7f843c2abfd
LauriESB <ich.lauri@gmail.com>
2024-12-09 22:41:05 -0300

    allow users to edit expense

--- porkin/src/main/java/com/porkin/porkin/security/SecurityConfig.java
+++ porkin/src/main/java/com/porkin/porkin/security/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
     .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
     .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
     .requestMatchers(HttpMethod.POST, "/friendRequest").permitAll()
-    .requestMatchers(HttpMethod.PUT, "/expense").hasRole("ADMIN")
+    .requestMatchers(HttpMethod.PUT, "/expense").permitAll()
     .anyRequest().authenticated())
     .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
     .build();
